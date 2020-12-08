380ba9ab9f6dbda3f90a52825a52b1c3f1791746
Nucleas <pro4medo@gmail.com>
2020-12-08 18:53:06 +0000

    add app run

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, GH Codespaces!"
+    return {"message": "Hello, GH Codespaces!"}, 200
+
+app.run(host='0.0.0.0', port=8080, debug=False)
